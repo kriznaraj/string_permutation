b6c18ec3cb647567b77fa88b17cf9275c5a19411
Kvenkatesan <Kvenkatesan@travelrepublic.co.uk>
2017-07-04 16:12:00 +0100

    Adding combinations for the text

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace code
     {
         static void Main(string[] args)
         {
-            DoPermute(args[0]);
+            Do(args[0]);
         }
 
-        static void DoPermute(string text)
+        static void Do(string text)
         {
             var dict = new Dictionary<char,int>();
             foreach (var item in text)
@@ -21,12 +21,17 @@ namespace code
                 else dict.Add(item, 1);
             }
 
+            Console.WriteLine("Permutations of given text {0}", text);
             Permute(dict.Keys.ToArray(), dict.Values.ToArray(), new char[text.Length], 0);
+            
+            Console.WriteLine("Combinations of given text {0}", text);
+            Combination(dict.Keys.ToArray(), dict.Values.ToArray(), new char[text.Length], 0, 0);
         }
 
         static void Permute(char[] text, int[] count, char[] res, int level) 
         {
-            if(level == res.Length){
+            if(level == res.Length)
+            {
                 Print(res);
                 return;
             }
@@ -45,9 +50,29 @@ namespace code
             }
         }
 
-        static void Print(char[] res) 
+        static void Combination(char[] text, int[] count, char[] res, int level, int pos)
         {
-            Console.WriteLine(res);
+            for (int i = pos; i < text.Length; i++)
+            {
+                if (count[i] == 0)
+                {
+                    continue;
+                }
+
+                res[level] = text[i];
+                Print(res, level + 1);
+                count[i]--;
+                Combination(text, count, res, level + 1, i);
+                count[i]++;
+            }
+        }
+
+        static void Print(char[] res, int index = -1) 
+        {
+            if(index > 0 && index < res.Length)
+                Console.WriteLine(res.SkipLast(res.Length - index).ToArray());
+            else
+                Console.WriteLine(res);
         }
     }
 }
